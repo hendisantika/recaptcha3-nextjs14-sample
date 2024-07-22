2ad87f1c18044d60d02af3a03cec27c2e20981e7
hendisantika <hendisantika@yahoo.co.id>
2024-07-22 19:58:48 +0700

    Modify page.tsx 🫸🌀✏️📗🐧🐳

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,128 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import React, {useState} from "react";
+import {useGoogleReCaptcha} from "react-google-recaptcha-v3";
+import axios from "axios";
+import GoogleCaptchaWrapper from "@/app/google-captcha-wrapper";
+
+interface PostData {
+  gRecaptchaToken: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  hearFromSponsors: boolean;
+}
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <GoogleCaptchaWrapper>
+        <HomeInside/>
+      </GoogleCaptchaWrapper>
+  );
+}
+
+function HomeInside() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [hearFromSponsors, setHearFromSponsors] = useState(false);
+  const [notification, setNotification] = useState('');
+
+  const {executeRecaptcha} = useGoogleReCaptcha();
+
+  const handleSubmitForm = function (e: any) {
+    e.preventDefault();
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not available yet");
+      setNotification(
+          "Execute recaptcha not available yet likely meaning key not recaptcha key not set"
+      );
+      return;
+    }
+    executeRecaptcha("enquiryFormSubmit").then((gReCaptchaToken) => {
+      submitEnquiryForm(gReCaptchaToken);
+    });
+  };
+
+  const submitEnquiryForm = (gReCaptchaToken: string) => {
+    async function goAsync() {
+      const response = await axios({
+        method: "post",
+        url: "/api/contact",
+        data: {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          hearFromSponsors: hearFromSponsors,
+          gRecaptchaToken: gReCaptchaToken,
+        },
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      });
+
+
+      if (response?.data?.success === true) {
+        setNotification(`Success with score: ${response?.data?.score}`);
+      } else {
+        setNotification(`Failure with score: ${response?.data?.score}`);
+      }
+    }
+
+    goAsync().then(() => {
+    }); // suppress typescript error
+  };
+
+  return (
+      <div className="container">
+        <main className="mt-5"> {/* Add a top margin for better spacing */}
+          <h2>Interested in Silicon Valley Code Camp</h2>
+          <form onSubmit={handleSubmitForm}>
+            <div className="mb-3">
+              <input
+                  type="text"
+                  name="firstName"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e?.target?.value)}
+                  className="form-control"
+                  placeholder="First Name"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                  type="text"
+                  name="lastName"
+                  value={lastName}
+                  onChange={(e) => setLastName(e?.target?.value)}
+                  className="form-control"
+                  placeholder="Last Name"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e?.target?.value)}
+                  className="form-control"
+                  placeholder="Email Address"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                  type="checkbox"
+                  name="hearFromSponsors"
+                  checked={hearFromSponsors}
+                  onChange={(e) => setHearFromSponsors(e?.target?.checked)}
+                  className="form-check-input"
+              />
+              <label className="form-check-label">Hear from our sponsors</label>
+            </div>
+            <button type="submit" className="btn btn-light">Submit</button>
+            {notification && <p className="mt-3 text-info">{notification}</p>}
+          </form>
+        </main>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
